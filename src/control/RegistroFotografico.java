f9afba384d2ed5ccd2233cd19ee229e5c68b682d
ana vega <>
2022-08-14 18:01:48 -0500

    Método para el registro fotográfico

--- src/control/RegistroFotografico.java
+++ src/control/RegistroFotografico.java
@@ -6,12 +6,80 @@
 package control;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
+import javax.swing.JFileChooser;
+import java.io.*;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
 
 /**
  *
  * @author jpag0
  */
 public class RegistroFotografico {
-    public ArrayList<File> imagenes = new ArrayList();
+   // public ArrayList<File> imagenes = new ArrayList();
+    JFileChooser seleccionar = new JFileChooser();
+    File archivo;
+    byte[] imagen;
+    FileInputStream entrada;
+    FileOutputStream salida;
+   
+    public RegistroFotografico(){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+    public byte [] AbrirImagen(File archivo){
+        byte[] imagenes = new byte[1024*100];
+        try{
+            entrada= new FileInputStream(archivo);
+            entrada.read(imagenes);
+            
+        }catch (Exception e){
+        }
+            return imagenes;
+    }
+
+    public String GuardarImagen(File archivo, byte[] byteImg){
+        String respuesta = null;
+        try {
+            salida = new FileOutputStream(archivo);
+            salida.write(byteImg);
+            respuesta = "La imagen se guardó correctamente";
+            
+        } catch (Exception e) {
+        }
+        return respuesta;
+        
+    }
+    
+      private void btnAgregarRegistro(java.awt.Event evt){
+          if(seleccionar.showDialog(null, null)==JFileChooser.APPROVE_OPTION){
+              archivo=seleccionar.getSelectedFile();
+              if(archivo.canRead()){
+                  if(archivo.getName().endsWith("jpg")||archivo.getName().endsWith("png")||archivo.getName().endsWith("gif"));
+                  imagen=AbrirImagen(archivo);                  
+              }else{
+                  JOptionPane.showMessageDialog(null, "Archivo no compatible");
+              }
+              if(seleccionar.showDialog(null, "Guardar")==JFileChooser.APPROVE_OPTION){
+                  archivo=seleccionar.getSelectedFile();
+                  if(archivo.getName().endsWith("jpg")||archivo.getName().endsWith("png")||archivo.getName().endsWith("gif"));
+                  String respuesta=GuardarImagen(archivo, imagen);
+                  if(respuesta!=null){
+                      JOptionPane.showMessageDialog(null, respuesta);
+                      
+                  }else{
+                      JOptionPane.showMessageDialog(null, "Archivo no guardado");
+                  }
+              }else{
+                  JOptionPane.showMessageDialog(null, "Archivo guardado");
+              }
+          }
+          
+      }      
 }
